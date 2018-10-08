91430fac6756e9a114ab1eab4c259a840e2b2003
Yuji YAMAMOTO <oo_hayjp@yahoo.co.jp>
2018-10-08 19:13:16 +0900

    DIC ph4:057061 リファクタリング実践

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,66 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @blogs = Blog.latest
+  end
+
+  # GET /blogs/1
+  # GET /blogs/1.json
+  def show
+  end
+
+  # GET /blogs/new
+  def new
+    @blog = Blog.new
+  end
+
+  # GET /blogs/1/edit
+  def edit
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+
+    respond_to do |format|
+      if @blog.save
+        ContentCleanupService.new(@blog).call
+        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @blog.update(blog_params)
+        ContentCleanupService.new(@blog).call
+        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  # DELETE /blogs/1
+  # DELETE /blogs/1.json
+  def destroy
+    @blog.destroy
+    respond_to do |format|
+      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_blog
+      @blog = Blog.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def blog_params
+      params.require(:blog).permit(:title, :content)
+    end
+end
---
+++ app/models/topic.rb
@@ -0,0 +1,3 @@
+class Topic < ApplicationRecord
+  include CommonModule
+end
